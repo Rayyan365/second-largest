554f0e550330f7cb84b636707010804fe783e0ec
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 00:51:06 -0700

    second-largest assignment done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+let arr = [ 0, 0, 9, 8, 6, 5, 4, 1, 1];
+
+let n = arr.length;
+
+let highest= -Infinity;
+let secondhighest= -Infinity;
+
+for (let i=0; <n; i++){
+    highest = Math.max (highest, arr[i]);
+}
+for (let i=0; i<n; 1++){
+    if (arr[i]< highest{
+        secondhighest = Math.max (secondhighest, arr[i]);
+    }    )
+}
+console.log(highest,secondhighest);
